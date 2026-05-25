229889f88a06685adb912d5dd2189552066791e8
Xynon <63993670+Xynonners@users.noreply.github.com>
2026-05-25 02:33:40 -0700

    pin dev sealing key

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [ -n "$PLAP_SEALING_KEYS" ] || [ "$PLAP_SEALING_KEYS" = "" ]; then
+	export PLAP_SEALING_KEYS='["aSWxI8Aqwo-Mzsfd1Nvtmp2-OVdGki-9RA--_aJ_on8"]'
+	echo "Using default sealing key for dev"
+fi
+
 set -euo pipefail
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
